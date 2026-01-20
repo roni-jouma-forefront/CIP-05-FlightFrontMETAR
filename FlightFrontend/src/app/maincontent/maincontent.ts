b7a5d4adf0a5ec57ff56c860e983c9faa5189cf3
Anna Hallberg <anna.hallberg@sveaskog.se>
2026-01-20 16:21:32 +0100

    Refactor code structure for improved readability and maintainability

--- FlightFrontend/src/app/maincontent/maincontent.ts
+++ FlightFrontend/src/app/maincontent/maincontent.ts
@@ -5,9 +5,8 @@ import {MatIconModule} from '@angular/material/icon';
 import {FormsModule} from '@angular/forms';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-
-////
 import { HttpClient } from '@angular/common/http';
+import * as Papa from "papaparse";
 
 
 @Component({
@@ -18,14 +17,63 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./maincontent.scss']
 })
 export class Maincontent {
+  airports: any[] = [];
   data: string | undefined; 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    this.loadCSV();
+  }
 
+  //sköter dropdownen i inputen så att den 
+dropdown() {
+  const dropdownEl = document.getElementById("myDropdown");
+  if (dropdownEl) {
+    dropdownEl.classList.toggle("show");
+  }
+}
+
+//placerar vald essa i input-rutan och stänger den
+selectAirport(airport: any) {
+  const inputEl = document.getElementById("icao-input") as HTMLInputElement;
+  if (inputEl) {
+    inputEl.value = airport.ident;
+  }
+  const dropdownEl = document.getElementById("myDropdown");
+  if (dropdownEl) {
+    dropdownEl.classList.remove("show");
+  }
+}
+
+//hämtar csvc-filen. OBS ska bytas ut mot hämtning i backenden
+  loadCSV() {
+    this.http.get('assets/airports.csv', { responseType: 'text' })
+      .subscribe(data => {
+        Papa.parse(data, {
+          header: true,
+          complete: (result: { data: any[]; }) => {
+            const allowedTypes = ['large_airport', 'medium_airport'];
+            
+            this.airports = result.data
+              .filter(airport => allowedTypes.includes(airport.type))
+              .map(airport => ({
+                ident: airport.ident,
+                name: airport.name,
+                municipality: airport.municipality,
+                type: airport.type
+              }));
+            
+            console.log(this.airports);
+          }
+        });
+      });
+    }
+
+    //hämtar inmatad ICAOkod samt felmeddelande vid felaktigt format
   runICAO(): void {
     const inputEl = document.getElementById("icao-input") as HTMLInputElement;
     const icao = inputEl.value.trim().toUpperCase();
 
-    if(!/^[A-Za-z]+$/.test(icao)){
+    //Felmeddelande vi felaktig inpout
+    if(!/^[A-Za-z0-9]+$/.test(icao)){
       window.alert("Ogiltigt ICAO-kodformat, testa igen")
       return;
     }
@@ -33,6 +81,7 @@ export class Maincontent {
     this.fetchMetarForICAO(icao);
   }
 
+  //hämtning av koden OBS implementera från BE
   fetchMetarForICAO(icao: string) {
     console.log("Här är koden: " + icao)
     const response = "METAR ESSA 191420Z 19006KT 9999 BKN010 00/M02 Q1029 TEMPO BKN009"
